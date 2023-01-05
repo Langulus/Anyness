75ab9b4f01899cc6a046a812dfb5353e7e0f59b2
Epixu <dimomarkov@gmail.com>
2023-01-05 20:20:44 +0200

    CMakeLists critical fixes

---
+++ LangulusUtilities.cmake
@@ -0,0 +1,32 @@
+function(langulus_init_git_submodule NAME)
+	if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${NAME}/.git" )
+        # Submodule hasn't been initialized yet, so call git submodule update --init on it
+        message(STATUS "Initializing submodule: ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}...")
+        find_package(Git REQUIRED)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} submodule update --init -- ${NAME} 
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+	endif()
+endfunction()
+
+function(langulus_import_framework)
+    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS OFF PARENT_SCOPE)
+    remove_definitions(-DLANGULUS_EXPORT_ALL)
+    if(NOT WIN32)
+        add_compile_options(-fvisibility=hidden)
+    endif()
+endfunction()
+
+function(langulus_copy_dlls TO TARGET FROM)
+    if(WIN32)
+        foreach(element ${ARGN})
+            add_custom_command(
+                TARGET ${TARGET} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE:${element}>" "$<TARGET_FILE_DIR:${TARGET}>"
+                COMMENT "Copying `$<TARGET_FILE:${element}>` to `$<TARGET_FILE_DIR:${TARGET}>`"
+            )
+        endforeach()
+    endif()
+endfunction()
